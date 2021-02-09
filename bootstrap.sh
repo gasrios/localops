d86a21325441947396ab0c4fa4b634fcf52a7ca2
Guilherme Rios <gasrios@gmail.com>
2021-02-08 21:44:17 -0300

    [issues/11] Support better security through greater flexibility

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,23 +2,29 @@
 
 set -eux
 
-export PYTHON_VERSION=3.9.1
-export DEBIAN_FRONTEND=noninteractive
-
-sudo apt update || yes
-
-sudo apt upgrade --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" || yes
-
-sudo apt install direnv --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" || yes
-
 if [ -z "$(which ansible)" ]
 then
-    DISTRO=$(egrep '^ID=' /etc/os-release | sed 's/^ID="\?\([^"]*\)"\?/\1/')
-    DISTRO_VERSION=$(egrep '^VERSION_ID=' /etc/os-release | sed 's/^VERSION_ID="\?\([^"]*\)"\?/\1/')
-    wget -SO- https://raw.githubusercontent.com/gasrios/p4lo/main/repository/${DISTRO}-${DISTRO_VERSION}-${PYTHON_VERSION}.tbz \
-    | tar xvj --directory ${HOME}
-    PATH=${HOME}/.local/bin:${HOME}/.python-${PYTHON_VERSION}/bin:$PATH
-    pip install --no-cache-dir --upgrade pip
-    pip install ansible
-    ./localops-cli.sh install.yaml
+    if [ "$(whoami)" = root -o ! -z "$(groups | egrep sudo)" ]
+    then
+        export PYTHON_VERSION=3.9.1
+        export DEBIAN_FRONTEND=noninteractive
+
+        $(which sudo) apt update
+        $(which sudo) apt upgrade --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+        $(which sudo) apt install direnv --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+        DISTRO=$(egrep '^ID=' /etc/os-release | sed 's/^ID="\?\([^"]*\)"\?/\1/')
+        DISTRO_VERSION=$(egrep '^VERSION_ID=' /etc/os-release | sed 's/^VERSION_ID="\?\([^"]*\)"\?/\1/')
+        wget -SO- https://raw.githubusercontent.com/gasrios/p4lo/main/repository/${DISTRO}-${DISTRO_VERSION}-${PYTHON_VERSION}.tbz \
+        | $(which sudo) tar xvj --directory /
+    fi
+
+    if [ -e /opt/python-${PYTHON_VERSION}/bin/ansible ]
+    then
+        export PATH=/opt/python-${PYTHON_VERSION}/bin:$PATH
+    else
+        echo "Ansible is not installed and cannot be installed by user $(whoami). Install it and try again."
+        false
+    fi
 fi
+
+./localops-cli.sh install.yaml
