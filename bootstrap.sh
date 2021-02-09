9805f7d65461f941d612c0089ee24edf7615d544
Guilherme Rios <gasrios@gmail.com>
2021-02-08 21:53:37 -0300

    [issues/11] Support better security through greater flexibility

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,16 +4,18 @@ set -eux
 
 if [ -z "$(which ansible)" ]
 then
+    export PYTHON_VERSION=3.9.1
+
     if [ "$(whoami)" = root -o ! -z "$(groups | egrep sudo)" ]
     then
-        export PYTHON_VERSION=3.9.1
         export DEBIAN_FRONTEND=noninteractive
-
         $(which sudo) apt update
         $(which sudo) apt upgrade --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
         $(which sudo) apt install direnv --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
+
         DISTRO=$(egrep '^ID=' /etc/os-release | sed 's/^ID="\?\([^"]*\)"\?/\1/')
         DISTRO_VERSION=$(egrep '^VERSION_ID=' /etc/os-release | sed 's/^VERSION_ID="\?\([^"]*\)"\?/\1/')
+
         wget -SO- https://raw.githubusercontent.com/gasrios/p4lo/main/repository/${DISTRO}-${DISTRO_VERSION}-${PYTHON_VERSION}.tbz \
         | $(which sudo) tar xvj --directory /
     fi
