18b261880baec0da3dd80995605de57e9ac6168e
Guilherme Rios <gasrios@gmail.com>
2024-05-04 18:23:03 +0200

    Revamped installer that supports both Debian and Ubuntu

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,68 +1,152 @@
 #!/usr/bin/env sh
 
-set -eux
+set -eu
 
-#
-# Distribution dependent setup goes in this function
-#
-distro_dependent_setup() {
-  DISTRO=$(egrep '^ID=' /etc/os-release | sed 's/^ID="\?\([^"]*\)"\?/\1/')
+BRANCH='main'
+DRY_RUN=''
+LOCALOPS_HOME="${HOME}/.localops"
+SUPERUSER=true
+WITH_DEPS=true
 
-  case "${DISTRO}" in
-  ubuntu)
-    export DEBIAN_FRONTEND=noninteractive
-    $(which sudo) apt update
-    $(which sudo) apt upgrade --assume-yes -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold"
-    $(which sudo) apt install --assume-yes python3-pip python-is-python3 git
-  ;;
-  *)
-    echo "Unsupported distro ${DISTRO}"
-    false
-  ;;
-  esac
+main() {
+
+  parse_arguments "${@}"
+
+  check_overwrite_previous_install
+
+  if [ "${WITH_DEPS}" = false ]; then
+    echo 'WARNING: skipping dependency installation.'
+  else
+    install_dependencies
+  fi
+
+  install_localops
+
 }
 
-BRANCH='master'
-
-while [ "${1:-}" != "" ]; do
-    case $1 in
-        -b | --branch )   shift
-            BRANCH=$1
-            ;;
+parse_arguments() {
+  while [ "${1:-}" != "" ]; do
+    case ${1} in
+    -b | --branch)
+      shift
+      BRANCH=${1}
+      ;;
+    -d | --debug)
+      set -x
+      ;;
+    -r | --dry-run)
+      DRY_RUN='echo'
+      ;;
+    -s | --superuser)
+      shift
+      SUPERUSER=${1}
+      ;;
+    -w | --with-deps)
+      shift
+      WITH_DEPS=${1}
+      ;;
     esac
     shift
-done
+  done
+}
 
-if [ -z "$(which ansible-playbook)" ]
-then
-  if [ -z "$(which pip3 || which pip)" ]
-  then
-    if [ "$(whoami)" = root -o ! -z "$(groups | egrep sudo)" ]
-    then
-      distro_dependent_setup
-    else
-      echo "Neither Ansible nor pip are installed, and cannot be installed by user $(whoami)."
-      false
+check_overwrite_previous_install() {
+
+  if [ -e "${LOCALOPS_HOME}" ]; then
+
+    read -p "${LOCALOPS_HOME} already exists and will be deleted. Proceed? [y/N] " CONTINUE
+
+    if [ "${CONTINUE}" != 'y' -a "${CONTINUE}" != 'Y' ]; then
+      echo 'Exiting.'
+      exit 1
     fi
+
   fi
-  export PATH=${HOME}/.local/bin:${PATH}
-  $(which pip3 || which pip) install --no-cache-dir --upgrade --force-reinstall --user --break-system-packages pip
-  pip install --no-cache-dir --upgrade --force-reinstall --user --break-system-packages wheel
-  pip install --no-cache-dir --upgrade --force-reinstall --user --break-system-packages ansible
-fi
 
-cd ~
-rm -rf .localops
+}
 
-git clone https://github.com/gasrios/localops.git .localops
+install_dependencies() {
 
-cd ~/.localops
-git checkout $BRANCH
-rm -rf .git*
+  # See https://0pointer.de/blog/projects/os-release
+  #
+  # Sources /etc/os-release, which sets an "ID" variable to a string identifying
+  # the distro
+  if [ ! -f '/etc/os-release' ]; then
+    echo '/etc/os-release not found. Exiting.'
+    exit 1
+  else
+    . /etc/os-release
+  fi
 
-if [ "$(whoami)" = root -o ! -z "$(groups | egrep sudo)" ]
-then
-  ./localops.sh root/install.yaml
-fi
+  if ! command -v git >/dev/null; then
+    echo 'git not found, installing...'
+    install_package git
+  fi
 
-./localops.sh user/install.yaml
+  if ! command -v ansible-playbook >/dev/null; then
+
+    echo 'ansible-playbook not found, installing...'
+
+    if ! command -v pip >/dev/null; then
+      echo 'pip not found, installing...'
+      install_package pip
+    fi
+
+    ${DRY_RUN} pip install --no-cache-dir --upgrade --force-reinstall --user --break-system-packages ansible
+
+    if ! echo "${PATH}" | grep -q "${HOME}/.local/bin"; then
+      export PATH=${HOME}/.local/bin:${PATH}
+    fi
+
+  fi
+
+}
+
+install_package() {
+
+  if [ "${SUPERUSER}" = false ]; then
+    echo "WARNING: skipping installation of package \"${1}\" (no superuser)."
+  else
+    case ${ID} in
+    ubuntu | debian)
+      COMMAND="${DRY_RUN} apt install --assume-yes ${1}"
+      ;;
+    *)
+      echo "Unsupported distribution \"${ID}\". Exiting."
+      exit 1
+      ;;
+    esac
+
+    execute_command "${COMMAND}"
+  fi
+
+}
+
+execute_command() {
+
+  if [ "$(whoami)" = root ]; then
+    ${1}
+    exit 0
+  fi
+
+  if [ ! -z "$(groups | egrep sudo)" ]; then
+    sudo ${1}
+    exit 0
+  fi
+
+  su -c "${1}"
+
+}
+
+install_localops() {
+
+  ${DRY_RUN} rm -rf "${LOCALOPS_HOME}"
+  ${DRY_RUN} git clone https://github.com/gasrios/localops.git "${LOCALOPS_HOME}"
+  ${DRY_RUN} cd "${LOCALOPS_HOME}"
+  ${DRY_RUN} git checkout ${BRANCH}
+  ${DRY_RUN} rm -rf .git*
+  ${DRY_RUN} ./localops.sh ./install.yaml
+
+}
+
+main "${@}"
--- localops.sh
+++ localops.sh
@@ -2,8 +2,22 @@
 
 set -eu
 
-while [ "$#" -gt 0 ]
-do
-  ANSIBLE_CONFIG='~/.localops/ansible.cfg' ansible-playbook -i localhost, -c local -e "ansible_python_interpreter=$(which python3 || which python)" $1
+ANSIBLE_CONFIG="${HOME}/.localops/ansible.cfg"
+
+while [ "$#" -gt 0 ]; do
+  if [ ! -z "$(groups | egrep sudo)" ]; then
+    # We can delegate prompting for a password to sudo, if the user is a member of the sudo group,
+    # with the advantage that there will be no prompt if NOPASSWD is set.
+    ANSIBLE_CONFIG="${ANSIBLE_CONFIG}" ansible-playbook -i localhost, -c local\
+      -e "ansible_python_interpreter=$(which python3 || which python)" $1
+  else
+    # If the user is not a member of sudo, we have to prompt for a password here.
+    # 
+    # TODO: this will always prompt for a password, even if changes are only local.
+    #
+    # TODO: if a playbook needs to impersonate two users or more, it won't work.
+    ANSIBLE_CONFIG="${ANSIBLE_CONFIG}" ansible-playbook -i localhost, -c local\
+      -e "ansible_python_interpreter=$(which python3 || which python)" --ask-become-pass $1
+  fi
   shift
 done
